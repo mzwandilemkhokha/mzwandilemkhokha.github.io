5a8d3a58666183b7ce77e9005f27e635925107ac
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-06-22 12:49:06 +0200

    22 june

--- design.js
+++ design.js
@@ -1,6 +1,6 @@
 /*Toggle between showing and hiding the navigation menu*/
 function myFunction() {
-    var x = document.getElementById("myLinks");
+    var x = document.getElementById("mylinks");
     if (x.style.display === "block") {
         x.style.display === "none"
     }
