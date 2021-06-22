0007955b6ce261f2abc1038e1877d99a232e08d5
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-06-22 15:47:03 +0200

    22 june

--- design.js
+++ design.js
@@ -2,10 +2,10 @@
 function myFunction() {
     var x = document.getElementById("mylinks");
     if (x.style.display === "block") {
-        x.style.display === "none"
+        x.style.display = "none"
     }
     else {
-        x.style.display === "block"
+        x.style.display ="block"
     }
 
 }
