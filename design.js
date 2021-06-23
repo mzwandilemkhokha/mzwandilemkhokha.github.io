8921cef4ff934ee874f186204bf446b704f78d79
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-06-23 17:49:21 +0200

    23 june

--- design.js
+++ design.js
@@ -12,10 +12,10 @@ function myFunction() {
 
 function myFunction() {
     var x = document.getElementById("Topnav");
-    if (x.className === "container topnav") {
-      x.className += "container responsive";
+    if (x.className === "topnav") {
+      x.className += "responsive";
     } else {
-      x.className = "container topnav";
+      x.className = "topnav";
     }
   }
 
