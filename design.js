b5c4b0e1fc7bf6bc7e5bd7aceeb7d0c4658decba
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-06-23 17:00:36 +0200

    23 june

--- design.js
+++ design.js
@@ -1,4 +1,4 @@
-/*Toggle between showing and hiding the navigation menu*/
+/*Toggle between showing and hiding the navigation menu
 function myFunction() {
     var x = document.getElementById("mylinks");
     if (x.style.display === "block") {
@@ -8,5 +8,14 @@ function myFunction() {
         x.style.display ="block"
     }
 
-}
+}*/
+
+function myFunction() {
+    var x = document.getElementById("Topnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
