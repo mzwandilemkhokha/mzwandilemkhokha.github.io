c95445e409ed304aff89d1291e09f18f4d955a8c
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-06-27 12:54:06 +0200

    27 june

--- design.js
+++ design.js
@@ -12,9 +12,9 @@ function myFunction() {
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className === "container topnav") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "container topnav";
   }
 }
