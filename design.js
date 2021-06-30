4ab5a5ad4f67b0d172aa61cd82f209bd58d179cf
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-06-30 22:38:20 +0200

    30 june

--- design.js
+++ design.js
@@ -12,7 +12,7 @@ function myFunction() {
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 30, 2021 15:37:25").getTime();
+var countDownDate = new Date("Jan 30, 2022 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
