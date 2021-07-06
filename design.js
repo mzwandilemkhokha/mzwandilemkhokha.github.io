3f00d5d6f925e273ab7ab1516e2d51446373c8a8
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-07-06 11:17:36 +0200

    6 july

--- design.js
+++ design.js
@@ -47,21 +47,32 @@ $(function () { // wait for document ready
 
   // define movement of panels
   var wipeAnimation = new TimelineMax()
-    .fromTo("section.panel.turqoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-    .fromTo("section.panel.green",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-    .fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+    // animate to second panel
+    .to("#slideContainer", 0.5, {z: -150})		// move back in 3D space
+    .to("#slideContainer", 1,   {x: "-25%"})	// move in to first panel
+    .to("#slideContainer", 0.5, {z: 0})				// move back to origin in 3D space
+    // animate to third panel
+    .to("#slideContainer", 0.5, {z: -150, delay: 1})
+    .to("#slideContainer", 1,   {x: "-50%"})
+    .to("#slideContainer", 0.5, {z: 0})
+    // animate to forth panel
+    .to("#slideContainer", 0.5, {z: -150, delay: 1})
+    .to("#slideContainer", 1,   {x: "-75%"})
+    .to("#slideContainer", 0.5, {z: 0});
 
   // create scene to pin and link animation
   new ScrollMagic.Scene({
       triggerElement: "#pinContainer",
       triggerHook: "onLeave",
-      duration: "300%"
+      duration: "500%"
     })
     .setPin("#pinContainer")
     .setTween(wipeAnimation)
     .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 });
+
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
