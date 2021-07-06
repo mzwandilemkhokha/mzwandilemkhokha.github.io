2f0feccf338db8a24c56eddda8a099a842faa580
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-07-06 09:33:17 +0200

    6 july

--- design.js
+++ design.js
@@ -40,6 +40,28 @@ var x = setInterval(function() {
   }
 }, 1000);
 
+//function for adding scroll panel
+$(function () { // wait for document ready
+  // init
+  var controller = new ScrollMagic.Controller();
+
+  // define movement of panels
+  var wipeAnimation = new TimelineMax()
+    .fromTo("section.panel.turqoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+    .fromTo("section.panel.green",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+    .fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+
+  // create scene to pin and link animation
+  new ScrollMagic.Scene({
+      triggerElement: "#pinContainer",
+      triggerHook: "onLeave",
+      duration: "300%"
+    })
+    .setPin("#pinContainer")
+    .setTween(wipeAnimation)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+});
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
