c30dfc2f38fa64c240fe31347556742aeac173b3
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-07-07 13:27:25 +0200

    7  july

--- anime.js
+++ anime.js
@@ -6,12 +6,12 @@ gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
   ease: "none",
   scrollTrigger: {
-    trigger: ".panelcont",
+    trigger: "#panelcont",
     pin: true,
     scrub: 1,
     snap: 1 / (sections.length - 1),
     // base vertical scrolling on how wide the container is so it feels more natural.
-    end: () => "+=" + document.querySelector(".panelcont").offsetWidth
+    end: () => "+=" + document.querySelector("#panelcont").offsetWidth
   }
 });
 
