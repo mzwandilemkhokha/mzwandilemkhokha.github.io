ac68273c871d04fc06bd34cba53ead7b01aa45a0
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-07-07 17:25:50 +0200

    7  july

--- anime.js
+++ anime.js
@@ -1,17 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray(".section appear");
+let sections = gsap.utils.toArray(".appear");
 
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
   ease: "none",
   scrollTrigger: {
-    trigger: ".section appear",
+    trigger: ".appear",
     pin: true,
     scrub: 1,
     snap: 1 / (sections.length - 1),
     // base vertical scrolling on how wide the container is so it feels more natural.
-    end: () => "+=" + document.querySelector(".section appear").offsetWidth
+    end: () => "+=" + document.querySelector(".appear").offsetWidth
   }
 });
 
