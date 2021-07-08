4e1bf998eabf245dc24abd5cf83431cd04a81693
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-07-08 13:17:43 +0200

    7  july

--- anime.js
+++ anime.js
@@ -6,7 +6,7 @@ gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
   ease: "none",
   scrollTrigger: {
-    trigger: "#anim",
+    trigger: ".section",
     pin: true,
     scrub: 1,
     snap: 1 / (sections.length - 1),
