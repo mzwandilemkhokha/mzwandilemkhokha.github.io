7ab75a15525cbe4e30092251b734f46aa49b5d1b
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-07-08 19:01:03 +0200

    8  july

--- anime.js
+++ anime.js
@@ -1,13 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
 const t1 =gsap.timeline();
-t1.to("#anim",5,{x:-window.innerWidth})
+t1.to(".anim",5,{x:-window.innerWidth})
 
-.from("#two",5,{x:-window.innerWidth*2})
-.to("#three",5,{x:-window.innerWidth*3})
-.from("#four",5,{x:-window.innerWidth*4})
+.from(".two",5,{x:-window.innerWidth*2})
+.to(".three",5,{x:-window.innerWidth*3})
+.from(".four",5,{x:-window.innerWidth*4})
 ScrollTrigger.create({
     animation: t1,
-    trigger:"#anim",
+    trigger:".anim",
     start:"center center",
     end: "+=4000",
     scrub: true,
