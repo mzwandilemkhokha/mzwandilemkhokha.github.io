c6b56a1ff4fda956f9d9e895ccd52003fdc73763
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-07-08 15:29:20 +0200

    7  july

--- anime.js
+++ anime.js
@@ -1,17 +1,16 @@
 gsap.registerPlugin(ScrollTrigger);
+const t1 =gsap.timeline();
+t1.to(".anim",5,{x:-window.innerWidth})
 
-let sections = gsap.utils.toArray(".section");
-
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#badgeinfo",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: () => "+=" + document.querySelector("#time").offsetWidth
-  }
-});
-
+t1.to(".two",5,{x:-window.innerWidth*2})
+t1.to(".three",5,{x:-window.innerWidth*3})
+t1.to(".four",5,{x:-window.innerWidth*4})
+ScrollTrigger.create({
+    animation: t1,
+    trigger:".anim",
+    start:"center center",
+    end: "+=4000",
+    scrub: true,
+    pin:true
+})
+/*old code*/ 
