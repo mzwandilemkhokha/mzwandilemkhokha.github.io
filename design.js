b26096b4194e9848d2f3674470883a36cce6e1ce
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-07-09 09:40:32 +0200

    anime

--- design.js
+++ design.js
@@ -32,6 +32,22 @@ var x = setInterval(function() {
   }
 }, 1000);
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+
 
 function myFunction() {
   var x = document.getElementById("myTopnav");
