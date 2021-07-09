0501c4be7db84ac3bed950d9fb32d233f44b91c1
Mzwandile Mkhokha <mzwandilemkhokha@yahoo.com>
2021-07-09 13:20:19 +0200

    anime

--- design.js
+++ design.js
@@ -37,7 +37,7 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("colactive");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
